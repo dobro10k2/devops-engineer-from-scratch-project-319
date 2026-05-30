4cc245f7cac0b86fb5a25b1df24ca9022babe2ed
Алексей Неустроев <hbg@yandex.ru>
2026-05-30 17:55:02 +0300

    style(terraform): format variables and outputs

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,37 +1,37 @@
 variable "cloud_id" {
   description = "Yandex Cloud ID"
-  type = string
+  type        = string
 }
 
 variable "folder_id" {
   description = "Yandex Cloud Folder ID"
-  type = string
+  type        = string
 }
 
 variable "yc_token" {
   description = "Yandex Cloud Token"
-  type      = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 variable "zone" {
   description = "Yandex Cloud Network Zone"
-  type    = string
-  default = "ru-central1-a"
+  type        = string
+  default     = "ru-central1-a"
 }
 
 variable "ssh_public_key" {
   description = "Yandex Cloud VM Public Key"
-  type = string
+  type        = string
 }
 
 variable "bucket_name" {
   description = "Yandex Cloud Bucket Name"
-  type    = string
-  default = "bulletin-board-storage"
+  type        = string
+  default     = "bulletin-board-storage"
 }
 
 variable "prometheus_workspace_id" {
   description = "Yandex Cloud Prometheus Workspace ID"
-  type = string
+  type        = string
 }
