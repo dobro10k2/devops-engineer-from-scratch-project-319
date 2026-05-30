754d2f5874c833c5f9fbfebef16bf78c24373b6b
Алексей Неустроев <hbg@yandex.ru>
2026-05-30 17:51:57 +0300

    fix(terraform): resolve tflint and formatting issues

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,35 +1,43 @@
 output "master_public_ip" {
+  description = "Public IP of the master node"
   value = yandex_compute_instance.master.network_interface[0].nat_ip_address
 }
 
 # Изменено: теперь выводит список IP-адресов всех воркеров
 output "worker_private_ips" {
+  description = "Worker private ips"
   value = yandex_compute_instance.worker[*].network_interface[0].ip_address
 }
 
 output "bucket_name" {
+  description = "Name of the created S3 bucket"
   value = yandex_storage_bucket.app_bucket.bucket
 }
 
 output "s3_access_key" {
+  description = "S3 access key"
   value     = yandex_iam_service_account_static_access_key.storage_key.access_key
   sensitive = true
 }
 
 output "s3_secret_key" {
+  description = "S3 secret key"
   value     = yandex_iam_service_account_static_access_key.storage_key.secret_key
   sensitive = true
 }
 
 output "lockbox_secret_id" {
+  description = "Lockbox secret ID"
   value = yandex_lockbox_secret.app.id
 }
 
 output "log_group_id" {
+  description = "Log group ID"
   value = yandex_logging_group.k8s_logs.id
 }
 
 output "observability_api_key" {
+  description = "Observality API key"
   value     = yandex_iam_service_account_api_key.observability_key.secret_key
   sensitive = true
 }
@@ -56,6 +64,7 @@ output "prometheus_console_url" {
 }
 
 output "eso_sa_key_json" {
+  description = "ESO SA key JSON"
   value = jsonencode({
     id                 = yandex_iam_service_account_key.eso_auth_key.id
     service_account_id = yandex_iam_service_account_key.eso_auth_key.service_account_id
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.100.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
     }
   }
-  required_version = ">= 0.13"
 }
